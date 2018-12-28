7ad21f34c8fd3be6a1d4e7bf7ce5b8cd428a3ca8
Marc <zunly@hotmail.com>
2018-12-28 13:33:05 +0800

    利用mews/captcha添加注册的时候输入验证码的功能

---
+++ config/captcha.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+
+    //可以看到, 把数字1, 数字0, 字母L,O这些容易让人晕菜的字符都去掉了, 聪明啊
+    'characters' => '2346789abcdefghjmnpqrtuxyzABCDEFGHJMNPQRTUXYZ',
+
+    //不同的配置方案, 默认5个字母, 长宽,之类
+    'default'   => [
+        'length'    => 5,
+        'width'     => 120,
+        'height'    => 36,
+        'quality'   => 90,
+    ],
+
+    'flat'   => [
+        'length'    => 6,
+        'width'     => 160,
+        'height'    => 46,
+        'quality'   => 90,
+        'lines'     => 6,
+        'bgImage'   => false,
+        'bgColor'   => '#ecf2f4',
+        'fontColors'=> ['#2c3e50', '#c0392b', '#16a085', '#c0392b', '#8e44ad', '#303f9f', '#f57c00', '#795548'],
+        'contrast'  => -5,
+    ],
+
+    'mini'   => [
+        'length'    => 3,
+        'width'     => 60,
+        'height'    => 32,
+    ],
+
+    'inverse'   => [
+        'length'    => 5,
+        'width'     => 120,
+        'height'    => 36,
+        'quality'   => 90,
+        'sensitive' => true,
+        'angle'     => 12,
+        'sharpen'   => 10,
+        'blur'      => 2,
+        'invert'    => true,
+        'contrast'  => -5,
+    ]
+
+];
