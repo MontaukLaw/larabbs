ce00a25aa670354281dd99cd6925b8b03e74d471
Marc <zunly@hotmail.com>
2018-12-28 13:01:31 +0800

    make了一个auth, 几乎搞定所有session, 用户管理, 登陆, 全部事儿, 靠, 这也太简单了

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -4,7 +4,8 @@
         <a class="navbar-brand " href="{{ url('/') }}">
             LaraBBS
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
@@ -17,8 +18,33 @@
             <!-- 导航栏右边 -->
             <ul class="navbar-nav navbar-right">
                 <!-- 认证链接 -->
-                <li class="nav-item"><a class="nav-link" href="#">登录</a></li>
-                <li class="nav-item"><a class="nav-link" href="#">注册</a></li>
+                {{--<li class="nav-item"><a class="nav-link" href="{{route('login')}}">登录</a></li>--}}
+                {{--<li class="nav-item"><a class="nav-link" href="{{route('register')}}">注册</a></li>--}}
+                {{-- 如果是游客, 就保持原来的状态, 如果登陆了, 就无需再注册啊登陆之类, 直接显示下弹框--}}
+                @guest
+                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img src="https://iocaffcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60"
+                                 class="img-responsive img-circle" width="30px" height="30px">
+                            {{ Auth::user()->name }}
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="">个人中心</a>
+                            <a class="dropdown-item" href="">编辑资料</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" id="logout" href="#">
+                                <form action="{{ route('logout') }}" method="POST">
+                                    {{ csrf_field() }}
+                                    <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                                </form>
+                            </a>
+                        </div>
+                    </li>
+                @endguest
             </ul>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,31 @@
 //});
 
 Route::get('/', 'PagesController@root')->name('root');
+Route::get('/home', 'PagesController@root')->name('home');
+
+//这一句牛逼了, 相当于定义了用户身份验证路由, 注册相关路由, 密码重置相关路由, email验证相关路由.
+Auth::routes();
+
+//等于下面:
+//// 用户身份验证相关的路由
+//Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+//Route::post('login', 'Auth\LoginController@login');
+//Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+//
+//// 用户注册相关路由
+//Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+//Route::post('register', 'Auth\RegisterController@register');
+//
+//// 密码重置相关路由
+//Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+//Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+//Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+//Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+//
+//// Email 认证相关路由
+//Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+//Route::get('email/verify/{id}', 'Auth\VerificationController@verify')->name('verification.verify');
+//Route::get('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+//这一句跟上面的首页冲突, 直接铲掉
+//Route::get('/home', 'HomeController@index')->name('home');
