9e7bbfbc514e61f601c9375d16a3a85c2c748bc7
Marc <zunly@hotmail.com>
2018-12-28 23:00:50 +0800

    1. users/1里面加入了introduction跟create_at字段信息, 并且create_at是中文的.

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable implements MustVerifyEmailContract
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','create_at',
+        'name', 'email', 'password','create_at','introduction'
     ];
 
     /**
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -14,10 +14,11 @@
                 <img class="card-img-top" src="https://iocaffcdn.phphub.org/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/600/h/600" alt="{{ $user->name }}">
                 <div class="card-body">
                     <h5><strong>个人简介</strong></h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+                    <p>{{ $user->introduction }} </p>
                     <hr>
                     <h5><strong>注册于</strong></h5>
-                    <p>{{ $user->create_at }} </p>
+                    {{ $user->created_at->diffForHumans() }}
+                    {{--<p>{{ $user->create_at }} </p>--}}
                 </div>
             </div>
         </div>
