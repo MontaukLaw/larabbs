9afdfb60ac18ba939b7f3a71f216c03a390fdf30
Marc <zunly@hotmail.com>
2018-12-29 19:33:15 +0800

    使用debugbar查看数据库查询条数, 使用预加载提高系统性能

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,16 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    //这类function表达式一种关系, 即Topic是由归属的, 只属于一个类别, 同时只可能由一个发帖人.
+    //通过function调用可以获得对应的归属 例如 $topic->category, $topic->user
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
