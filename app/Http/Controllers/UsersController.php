4c93e788bde02c4a2eabf27cf193ee6b2dda18c6
Marc <zunly@hotmail.com>
2018-12-29 09:44:01 +0800

    上传头像图片

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -19,9 +20,26 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request,ImageUploadHandler $uploader, User $user)
     {
-        $user->update($request->all());
+        $data = $request->all();
+
+        //如果上传了头像
+        if ($request->avatar) {
+            //尝试保存头像并返回保存目录
+            $result = $uploader->save($request->avatar, 'avatars', $user->id);
+            //成功之后, 写入数组
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        //写库
+        $user->update($data);
+
+        //dd($request->avatar);
+        //$user->update($request->all());
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
+
 }
