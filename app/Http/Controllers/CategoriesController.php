a7a431247897b884424e7e657b76cfab16ea96a1
Marc <zunly@hotmail.com>
2018-12-29 22:15:24 +0800

    topic排序是在Model里面做的, 也是有一个类似提供者或者也可以说是工厂方法的, 分别根据request过来的要求switch排序方案

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -9,11 +9,18 @@ use App\Models\Topic;
 class CategoriesController extends Controller
 {
     //这个方法返回的是此分类下的所有文章列表, 并按默认20条每页分页.
-    public function show(Category $category)
+    public function show(Category $category,Request $request, Topic $topic)
     {
+
+        $topic=  Topic::with('user', 'category')->withOrder($request->order);
         // 读取分类 ID 关联的话题，并按每 20 条分页
-        $topics = Topic::where('category_id', $category->id)->paginate(20);
+        $topics = $topic->where('category_id', $category->id)->paginate(20);
         // 传参变量话题和分类到topics的index模板中
         return view('topics.index', compact('topics', 'category'));
+
+        // 读取分类 ID 关联的话题，并按每 20 条分页
+        //$topics = Topic::where('category_id', $category->id)->paginate(20);
+        // 传参变量话题和分类到topics的index模板中
+        //return view('topics.index', compact('topics', 'category'));
     }
 }
